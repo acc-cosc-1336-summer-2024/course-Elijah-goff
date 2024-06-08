a7210719ac705cba16ba8af8fe1de20e17572db1
Elijah-goff <elijah.goff@g.austincc.edu>
2024-06-08 20:44:24 +0000

    create multiply numbers function

--- src/examples/b_input_proc_output/input_process_output.py
+++ src/examples/b_input_proc_output/input_process_output.py
@@ -5,3 +5,8 @@ def display_output():
 def test_config():
     return True
 
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+    
--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,6 @@
-#
+import output
+whole_number = 30 #an integer variable 
+
+result = output.multiply_numbers(6,6)
+
+print(whole_number * 36)
