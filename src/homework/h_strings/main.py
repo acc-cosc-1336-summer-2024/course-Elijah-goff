a724d956c8ca1bd6c40ff9e80e7be46385dd42a6
Elijah-goff <elijah.goff@g.austincc.edu>
2024-07-07 05:19:55 +0000

    HW 5 DNA Domain

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,46 @@
-#
+from strings import get_hamming_distance, get_dna_complement
+
+def main_menu():
+
+    while True:
+        print("\nMenu")
+        print("1- Hamming Distance")
+        print("2- DNA Complement")
+        print("3-Exit Now")
+
+        choice = input("Enter a choice:")
+
+        if choice == "1":
+            dna1 = input("Enter first DNA string: ")
+            dna2 = input("Enter second DNA String: ")
+
+            try:
+
+              distance = get_hamming_distance(dna1, dna2)
+              print("Hamming Distance: " + str(distance))
+
+            except ValueError as e:
+                print("Error: " + str(e))
+
+        elif choice == "2":
+            dna = input("Enter DNA string: ")
+            complement = get_dna_complement(dna)
+            print("DNA Complement: " + complement)
+
+
+        elif choice == "3":
+            print("Exiting Program...")   
+            break 
+
+
+        else:
+            print("Invalid Option. Please choose option 1, 2 or 3.")
+
+
+    if __name__ == "__main__":
+        main_menu()
+
+
+
+
+
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,23 @@
 #
+from re import A
+
+
+def get_hamming_distance(dna1, dna2):
+    if len(dna1) != len(dna2):
+        raise ValueError('Strings must have identical length')
+    
+    distance = 0
+    for ch1, ch2 in zip(dna1, dna2):
+        if ch1 != ch2:
+         distance += 1
+    return distance 
+
+def get_dna_complement(dna):
+   complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+   reversed_dna = dna[::-1]
+   complement_dna = ''
+   complement_dna = ''.join(complement[ch] for ch in reversed_dna)
+      
+   return complement_dna
+
+
